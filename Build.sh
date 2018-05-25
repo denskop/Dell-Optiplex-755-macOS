2290d4073e24233ca69f33d211fc377040d2d627
denskop <denskop.dev@gmail.com>
2018-05-25 17:09:32 +0300

    Update ACPI patches

--- Build.sh
+++ Build.sh
@@ -2,6 +2,9 @@
 
 SELF_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# EC
+ADD_EC_DEVICE="Yes"
+
 # Additional paths
 ACPI_PATCHES_PATH="$SELF_PATH/AcpiPatches"
 BUILD_PATH="$SELF_PATH/Build"
@@ -44,7 +47,29 @@ $PATCH "$BUILD_PATH/Dsl/"DSDT.dsl "$ACPI_PATCHES_PATH/"DSDT_FixHPET.txt
 $PATCH "$BUILD_PATH/Dsl/"DSDT.dsl "$ACPI_PATCHES_PATH/"DSDT_FixIPIC.txt
 $PATCH "$BUILD_PATH/Dsl/"DSDT.dsl "$ACPI_PATCHES_PATH/"DSDT_FixMutex.txt
 $PATCH "$BUILD_PATH/Dsl/"DSDT.dsl "$ACPI_PATCHES_PATH/"DSDT_HideDevices.txt
+$PATCH "$BUILD_PATH/Dsl/"DSDT.dsl "$ACPI_PATCHES_PATH/"DSDT_RenameLPC.txt
 $PATCH "$BUILD_PATH/Dsl/"DSDT.dsl "$ACPI_PATCHES_PATH/"DSDT_Rename.txt
+
+# Deal with EC emulation
+if [ $ADD_EC_DEVICE = "Yes" ]; then
+    $PATCH "$BUILD_PATH/Dsl/"DSDT.dsl "$ACPI_PATCHES_PATH/"DSDT_PSKbAsEC.txt
+else
+    $PATCH "$BUILD_PATH/Dsl/"DSDT.dsl "$ACPI_PATCHES_PATH/"DSDT_HidePSKb.txt
+fi
+
+## Patch2 acpi tables
+
+# SSDT
+$PATCH "$BUILD_PATH/Dsl/"SSDT-0-st_ex.dsl "$ACPI_PATCHES_PATH/"DSDT_RenameLPC.txt
+
+# DSDT
+$PATCH2 "$BUILD_PATH/Dsl/"DSDT.dsl "$ACPI_PATCHES_PATH/"DSDT_Header.txt
+
+# SSDT
+$PATCH2 "$BUILD_PATH/Dsl/"SSDT-0-st_ex.dsl "$ACPI_PATCHES_PATH/"SSDT_External.txt
+$PATCH2 "$BUILD_PATH/Dsl/"SSDT-1-Cpu0Ist.dsl "$ACPI_PATCHES_PATH/"SSDT_Cpu0Ist.txt
+$PATCH2 "$BUILD_PATH/Dsl/"SSDT-2-Cpu1Ist.dsl "$ACPI_PATCHES_PATH/"SSDT_Cpu1Ist.txt
+$PATCH2 "$BUILD_PATH/Dsl/"SSDT-3-CpuPm.dsl "$ACPI_PATCHES_PATH/"SSDT_CpuPm.txt
 
 # More
 #$PATCH2 "$BUILD_PATH/Dsl/"APIC.dsl "$ACPI_PATCHES_PATH/"APIC.txt
